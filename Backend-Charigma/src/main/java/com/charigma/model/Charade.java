8b792e5d92c32b4a903879ee6a747e0b5b37a319
Daniela <danisegd@hotmail.com>
2024-03-26 17:13:29 -0300

    Create getter and setter of charade class

--- Backend-Charigma/src/main/java/com/charigma/model/Charade.java
+++ Backend-Charigma/src/main/java/com/charigma/model/Charade.java
@@ -1,13 +1,6 @@
 package com.charigma.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
-import java.util.List;
+import jakarta.persistence.*;
 
 @Entity(name = "charades")
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -15,11 +8,35 @@ public abstract class Charade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    @Column(length = 150)
     private String question;
+    @Column(length = 20)
     private String answer;
     private String difficulty;
     private String origin;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {return question;}
+
+    public void setQuestion(String question) {this.question = question;}
+
+    public String getAnswer() {return answer;}
+
+    public void setAnswer(String answer) {this.answer = answer;}
+
+    public String getDifficulty() {return difficulty;}
+
+    public void setDifficulty(String difficulty) {this.difficulty = difficulty;}
+
+    public String getOrigin() {return origin;}
+
+    public void setOrigin(String origin) {this.origin = origin;}
 }
