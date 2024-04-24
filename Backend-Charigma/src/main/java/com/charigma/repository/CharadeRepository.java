27a6a1eebdb8c2d4f802dc4a6033283bf135983f
Daniela <danisegd@hotmail.com>
2024-04-24 15:17:13 -0300

    Restart project with mysql as database

--- Backend-Charigma/src/main/java/com/charigma/repository/CharadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.charigma.repository;
-
-import com.charigma.model.Charade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CharadeRepository extends JpaRepository<Charade, Long> {
-}
